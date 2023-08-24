585e3ef5bad6b71b08bb37187450ee6f4a979c73
viknupp <arq.vitoriaknupp@gmail.com>
2023-08-24 14:42:00 +0100

    ref: Accordion to class

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,16 +1,27 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(".js_accordion dt");
-  const activeClass = "active";
-
-  if (accordionList.length) accordionList[0].classList.add(activeClass);
-  accordionList[0].nextElementSibling.classList.add(activeClass);
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "active";
   }
 
-  accordionList.forEach((item) => {
-    item.addEventListener("click", activeAccordion);
-  });
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  // add events to accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordion(item));
+    });
+  }
+
+  // init function
+  init() {
+    if (this.accordionList.length) {
+      // active first item
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import SmoothScroll from "./modules/smooth-scroll.js";
+import Accordion from "./modules/accordion.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
-import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -13,8 +13,10 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 const smoothScroll = new SmoothScroll('.js_menu a[href^="#"]');
 smoothScroll.init();
 
+const accordion = new Accordion(".js_accordion dt");
+accordion.init();
+
 initAnimaScroll();
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
