51b28cdccb34ac68ca6bde0501ad6a823f2f63c9
viknupp <arq.vitoriaknupp@gmail.com>
2023-08-29 20:17:25 +0100

    ref: new animaScroll to class

--- js/modules/anima-scroll.js
+++ js/modules/anima-scroll.js
@@ -3,23 +3,39 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.middleWindow = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.middleWindow < 0;
-      if (isSectionVisible) {
-        section.classList.add("active");
-      } else if (section.classList.contains("active")) {
-        section.classList.remove("active");
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.middleWindow),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("active");
+      } else if (item.element.classList.contains("active")) {
+        item.element.classList.remove("active");
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener("scroll", this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
