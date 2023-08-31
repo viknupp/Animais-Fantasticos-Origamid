32d33d8087910d7783db13d365e991e113b61553
viknupp <arq.vitoriaknupp@gmail.com>
2023-08-31 14:14:38 +0100

    fix: return this

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -9,19 +9,17 @@ export default class Accordion {
     item.nextElementSibling.classList.toggle(this.activeClass);
   }
 
-  // add events to accordion
   addAccordionEvent() {
     this.accordionList.forEach((item) => {
       item.addEventListener("click", () => this.toggleAccordion(item));
     });
   }
 
-  // init function
   init() {
     if (this.accordionList.length) {
-      // active first item
       this.toggleAccordion(this.accordionList[0]);
       this.addAccordionEvent();
     }
+    return this;
   }
 }
